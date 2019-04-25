c89b2e2bd381f9e46aff73df268a372c4f2948a9
mrfabroa <eric.fabroa@ycdsbk12.ca>
2019-04-25 00:46:48 -0400

    lab directories

---
+++ Lab02-DrawPicture/lab_02.py
@@ -0,0 +1,22 @@
+
+import arcade
+
+
+# create window
+arcade.open_window(600, 600, "My First Arcade Program")
+
+# set background colour
+arcade.set_background_color(arcade.color.WHITE)
+
+
+arcade.start_render()
+# all drawing commands BELOW this line -----------------------
+
+
+# put all drawing commands here
+
+# all drawing commands ABOVE this line  -----------------------------
+arcade.finish_render()
+
+
+arcade.run()
